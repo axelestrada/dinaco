55888db4d23b9edb4d0daa336ce3266f29559656
Axel Estrada <axele1524@gmail.com>
2026-07-29 11:36:19 -0600

    feat: implement header component into home screen

--- app/src/main/java/com/axelestrada/dinaco/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/axelestrada/dinaco/ui/screens/HomeScreen.kt
@@ -1,48 +1,22 @@
 package com.axelestrada.dinaco.ui.screens
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.axelestrada.dinaco.ui.components.NotificationsButton
-import com.axelestrada.dinaco.ui.theme.Typography
+import com.axelestrada.dinaco.ui.components.Header
+import com.axelestrada.dinaco.viewmodel.helper.getCurrentGreeting
 
 @Composable
 fun HomeScreen() {
+    val greeting = getCurrentGreeting()
+
     Column(
         modifier = Modifier.padding(24.dp)
     ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Column {
-                Text(
-                    text = "Hola, Axel",
-                    style = Typography.headlineMedium,
-                    color = MaterialTheme.colorScheme.onBackground
-                )
-
-                Spacer(modifier = Modifier.height(2.dp))
-
-                Text(
-                    text = "Buenos días",
-                    style = Typography.bodySmall,
-                    color = MaterialTheme.colorScheme.onSurfaceVariant
-                )
-            }
-
-            NotificationsButton()
-        }
+        Header(
+            title = "Hola, Axel", subtitle = greeting, showNotifications = true
+        )
     }
 }
